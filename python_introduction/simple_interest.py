99cc278489734ad9697699d825b1ae1822ec82f9
Shema Manasseh <shemamanase992@gmail.com>
2025-11-04 11:12:33 +0300

    Refactor variable names in simple_interest.py for consistency

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 #Objective: Apply basic Python arithmetic operations and variable assignments to calculate the simple interest on a given principal amount, rate of interest, and time.
 # simple_interest.py
 principal = 1000  # Principal amount in dollars
-rate_of_interest = 0.05  # Annual interest rate in percentage (5%)
-time_in_years = 3  # Time in years
+rate = 0.05  # Annual interest rate in percentage (5%)
+time = 3  # Time in years
 
-interest = principal * rate_of_interest * time_in_years
+interest = principal * rate * time
 
 print("The simple interest is: ", interest)
